a400ffaa3bbfff6fed117bd3ca1a0b4c20618d88
Matt Elstone <matthew_elstone@hotmail.com>
2021-10-26 14:49:04 +0000

    added validation

--- run.py
+++ run.py
@@ -34,6 +34,8 @@ def validate_data(values):
     or if there aren't exactly 6 values.
     """
     try:
+        [int(value) for value in values]
+        print(values)
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
